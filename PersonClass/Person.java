1beddde674d0772382b607e663d571397bd0d4fe
Cesar C. F <cafra3@icloud.com>
2021-04-19 17:17:25 -0500

    Person Class

--- PersonClass/Person.java
+++ PersonClass/Person.java
@@ -4,7 +4,7 @@ class Person {
     public Person(int ageParam, String nameParam) {
         this.age = ageParam;
         this.name = nameParam;
-    }
+    } 
     public void objectMethods(Person anotherObject) {
         System.out.println("Class name: " + this.getClass().getSimpleName());
         System.out.println("toString: " + this.toString());
