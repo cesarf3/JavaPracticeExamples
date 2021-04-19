4b745e5d5d1ebc82b8d18195999dadb4063077fc
Cesar C. F <cafra3@icloud.com>
2021-04-19 11:19:51 -0500

    Person Class

--- PersonClass/PersonTest.java
+++ PersonClass/PersonTest.java
@@ -2,7 +2,9 @@ class PersonTest {
     public static void main(String[] args) {
         Person person1 = new Person(10, "Person1");
         Person person2 = new Person(5, "Person2");
+        Person person3 = new Person(10, "Person1");
         person1.objectMethods(person2);
+        person1.objectMethods(person3);
     }
 }
 
